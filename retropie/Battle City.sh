3aa65ed2212acf92a33b722b3b5587da554bb53b
Roman Khomenko <dreamerixx@gmail.com>
2018-01-13 11:34:18 +0600

    Moving files

--- retropie/Battle City.sh
+++ retropie/Battle City.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-
-# SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-# cd $SCRIPT_DIR
-
 "/opt/retropie/supplementary/runcommand/runcommand.sh" 0 _PORT_ "battle-city" "/home/pi/RetroPie/roms/ports/battle-city/tanks.py"
