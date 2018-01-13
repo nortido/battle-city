f0280c081f6ed3e4736a2fff01bfc4513b4b89bb
Roman Khomenko <dreamerixx@gmail.com>
2018-01-14 00:20:21 +0600

    Updated  install script

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 # download the file
-sudo wget https://github.com/Nortido/battle-city/archive/master.zip
+echo "Download game..."
+wget --no-check-certificate https://github.com/Nortido/battle-city/archive/master.zip
+# curl -LOk https://github.com/Nortido/battle-city/archive/master.zip
 # unzip the file so that we can use the things inside of it
-sudo unzip master.zip -d /home/pi/RetroPie/roms/ports/battle-city
+echo "Unzip and move files..."
+unzip master.zip -d /home/pi/RetroPie/roms/ports/battle-city
 #move the files from the zip to the proper location
-sudo mv /home/pi/RetroPie/roms/ports/battle-city/battle-city-master*/* /home/pi/RetroPie/roms/ports/battle-city
-sudo rmdir /home/pi/RetroPie/roms/ports/battle-city/battle-city-master*
+mv "/home/pi/RetroPie/roms/ports/battle-city/battle-city-master/retropie/Battle City.sh" /home/pi/RetroPie/roms/ports
+mv /home/pi/RetroPie/roms/ports/battle-city/battle-city-master*/* /home/pi/RetroPie/roms/ports/battle-city
+rmdir /home/pi/RetroPie/roms/ports/battle-city/battle-city-master*
+rm /home/pi/RetroPie/roms/ports/master.zip
 # install pygame framework
+echo "Install pygame framework..."
 sudo apt-get install python-pygame
--- retropie/Battle City.sh
+++ retropie/Battle City.sh
@@ -1,2 +1,5 @@
-#!/bin/bash
-"/opt/retropie/supplementary/runcommand/runcommand.sh" 0 _PORT_ "battle-city" "/home/pi/RetroPie/roms/ports/battle-city/tanks.py"
+#!/usr/bin/env bash
+# "/opt/retropie/supplementary/runcommand/runcommand.sh" 0 _PORT_ "battle-city" "/home/pi/RetroPie/roms/ports/battle-city/tanks.py"
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+cd "$SCRIPT_DIR"
+python ./source/tanks.py
